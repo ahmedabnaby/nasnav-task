de45629ddb26eeae4f12fa5ca9e71d5097a7260c
Ahmed Abdelnaby <ahmedabnaby.97@gmail.com>
2021-08-23 01:52:33 +0200

    Added sidebar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,58 @@
 
 import React, { Component } from 'react';
+import Sidebar from "react-sidebar";
 import { Container } from 'react-bootstrap';
 class Home extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            sidebarOpen: false
+          };
+          this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
     }
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+      }
     render() { 
         return ( 
         <div>
             <Container fluid>
                 <div className="upperNav">
                 <ul>
-                    <li className="sidebarBtn"><a href="#"><img src={process.env.PUBLIC_URL + 'images/sidebar.svg'} role="..."/></a></li>
-                    <li className="logo"><img src={process.env.PUBLIC_URL + 'images/logo.svg'} role="..."/></li>
+                <Sidebar
+                    sidebar={
+                    <ul>
+                    <a href="#"><li className="logo-sidebar" style={{marginTop:'15px'}}><img src={process.env.PUBLIC_URL + 'images/logo.svg'} role="..."/></li></a>
+                        <a href="#" style={{textDecoration:'none'}}>
+                            <li style={{position:'relative', left:"-25px", top:'100px'}}>
+                            <img src={process.env.PUBLIC_URL + 'images/phone.svg'} role="..."/>
+                            <span style={{marginLeft:'5px', position:'relative', top:'-3px'}}>Contact Us</span>
+                            </li>
+                        </a>
+                        <a href="#" style={{textDecoration:'none'}}>
+                            <li style={{position:'relative', left:"-25px", top:'125px'}}>
+                            <img src={process.env.PUBLIC_URL + 'images/track.svg'} role="..."/>
+                            <span style={{marginLeft:'5px', position:'relative', top:'-3px'}}>Track Order</span>
+                            </li>
+                        </a>
+                        <a href="#" style={{textDecoration:'none'}}>
+                            <li style={{position:'relative', left:"-25px", top:'150px'}}>
+                            <img src={process.env.PUBLIC_URL + 'images/location.svg'} role="..."/>
+                            <span style={{marginLeft:'5px', position:'relative', top:'-3px'}}>Find A Store</span>
+                            </li>
+                        </a>
+                    </ul>
+                    }
+                    open={this.state.sidebarOpen}
+                    onSetOpen={this.onSetSidebarOpen}
+                    styles={{ 
+                        sidebar: { position:"fixed",background: "white"}
+                     }}
+                >
+                    <li  onClick={() => this.onSetSidebarOpen(true)} className="sidebarBtn"><a href="#"><img src={process.env.PUBLIC_URL + 'images/sidebar.svg'} role="..."/></a></li>
+                </Sidebar>
+                    
+                    <a href="#"><li className="logo"><img src={process.env.PUBLIC_URL + 'images/logo.svg'} role="..."/></li></a>
                     <li>
                         <div className="navText">
                         <img src={process.env.PUBLIC_URL + 'images/left-arrow.svg'} className="left-arrow" role="..."/>
@@ -36,7 +76,7 @@ class Home extends Component {
                         <a href="#">
                             <li className="location">
                             <img src={process.env.PUBLIC_URL + 'images/location.svg'} role="..."/>
-                            <span>Track Order</span>
+                            <span>Find A Store</span>
                             </li>
                         </a>
                     </ul>
